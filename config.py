41890f0bf726fee7caa56f38a8a44480592c933d
adityamuharram <99388738+adityamuharram@users.noreply.github.com>
2023-12-02 18:29:14 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import psycopg2
 
 conn = psycopg2.connect(
         host="localhost",
-        database="postgres",
+        database="mahasiswa",
         user='postgres',
         password='1234')
